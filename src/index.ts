112679b957bdb9869b3b8e9e233c0cbb7564f0e6
Selma Mikic <selma.mikic@gmail.com>
2021-08-29 11:58:52 +0200

    add map

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
-// import { User } from "./User";
-// import { Company } from "./Company";
+import { User } from "./User";
+import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 
-// const user = new User();
-// console.log(user);
+const user = new User();
+console.log(user);
 
-// const company = new Company();
-// console.log(company);
+const company = new Company();
+console.log(company);
 
-new google.maps.Map(document.getElementById("map"), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0
-  }
-});
+const customMap = new CustomMap("map");
+customMap.addMarker(user);
+customMap.addMarker(company);
