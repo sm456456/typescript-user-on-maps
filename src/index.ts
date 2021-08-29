1e725630cdbb946a27a73ac17afdda878eac3d61
Selma Mikic <selma.mikic@gmail.com>
2021-08-29 09:21:19 +0200

    add maps

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-import { User } from "./User";
-import { Company } from "./Company";
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-const user = new User();
-console.log(user);
+// const user = new User();
+// console.log(user);
 
-const company = new Company();
-console.log(company);
+// const company = new Company();
+// console.log(company);
+
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
